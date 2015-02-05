0e0ec6b8767b3cf27dc17d9e96723ce9cb13b146
n8fr8 <nathan@freitas.net>
2015-02-05 10:20:36 -0500

    add obfs4 pluto plugin

---
+++ plugins/obfs4/src/info/guardianproject/pluto/obfs4/PlutoActivity.java
@@ -0,0 +1,9 @@
+package info.guardianproject.pluto.meek;
+import android.app.Activity;
+
+/*
+ * this is an empty placeholder activity for PLUTO plugin
+ */
+public class PlutoActivity extends Activity {
+
+}
