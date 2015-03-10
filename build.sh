db478c64c9d15057997267abff422f7b2c5cfa44
n8fr8 <nathan@freitas.net>
2015-03-10 17:54:57 -0400

    adds Position Independent Exec flag for Android 4.1+

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export NDK_TOOLCHAIN=~/dev/android/ndk
+#export NDK_TOOLCHAIN=/please/define/this/to/your/ndk
+
 export CC=$NDK_TOOLCHAIN/bin/arm-linux-androideabi-gcc
 export THISPATH=`pwd`
 export GOROOT=$THISPATH/go-android/go
@@ -9,9 +10,12 @@ export GOOS=android
 export GOARCH=arm
 export GOARM=7
 export CGO_ENABLED=1
+GOGCCFLAGS="$GOGCCFLAGS -fPIE -pie"
 
 GO="$GOROOT/bin/go"
 
-$GO get git.torproject.org/pluggable-transports/meek.git/meek-client
+$GO env
 
-$GO get git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy
+$GO get  --ldflags '-extldflags "-fPIE -pie"' git.torproject.org/pluggable-transports/meek.git/meek-client
+$GO get  --ldflags '-extldflags "-fPIE -pie"' git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy 
+
