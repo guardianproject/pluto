c9f8799540f329480b084010efb354e6bac8af98
n8fr8 <nathan@freitas.net>
2015-03-19 09:10:32 -0400

    use the n8fr8 fork of meek-client with yawnings socks5 support

--- build.sh
+++ build.sh
@@ -16,6 +16,7 @@ GO="$GOROOT/bin/go"
 
 $GO env
 
-$GO get  --ldflags '-extldflags "-fPIE -pie"' git.torproject.org/pluggable-transports/meek.git/meek-client
+##$GO get  --ldflags '-extldflags "-fPIE -pie"' git.torproject.org/pluggable-transports/meek.git/meek-client
+$GO get  --ldflags '-extldflags "-fPIE -pie"' github.com/n8fr8/meek/meek-client
 $GO get  --ldflags '-extldflags "-fPIE -pie"' git.torproject.org/pluggable-transports/obfs4.git/obfs4proxy 
 
